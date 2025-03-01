41013677f28cd3dc3c17138bee0f9bf70c851efc
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-01 16:45:38 +0530

    [Om Datt Tripathi] Add. Display hello message in terminal

--- src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
@@ -1,13 +1,18 @@
 package com.demo.springConcepts;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringConceptsApplication {
+public class SpringConceptsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConceptsApplication.class, args);
 	}
 
+	@Override
+	public void run(String...args) throws Exception{
+		System.out.println("Hello world");
+	}
 }
