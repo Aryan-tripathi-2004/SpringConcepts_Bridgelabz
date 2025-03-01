5c7e18a16c06fc5ccf0768cce4452cc208929f70
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-01 16:38:49 +0530

    [Om Datt Tripathi] init. initial Build

---
+++ src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+
+}
